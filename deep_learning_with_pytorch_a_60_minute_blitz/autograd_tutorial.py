28bcc6b03dbd92ad98c8773210f81f7615ddccf3
damvantai <tai94bn@gmail.com>
2017-08-26 08:10:44 +0700

    o = 1/4 sum(3(x+2)^2) / dx = 3/2(x + 2)

---
+++ deep_learning_with_pytorch_a_60_minute_blitz/autograd_tutorial.py
@@ -0,0 +1,24 @@
+from torch.autograd import Variable
+
+import torch
+
+x = Variable(torch.ones(2, 2), requires_grad=True)
+print(x)
+
+y = x + 2
+print(y)
+
+# y was created as a result of an operation, so it has a grad_fn
+print(y.grad_fn)
+
+# do more operations on y
+z = y * y * 3
+out = z.mean()
+
+print(z, out)
+
+# Gradients
+out.backward()
+
+# print gradients d(out)/dx
+print(x.grad)
