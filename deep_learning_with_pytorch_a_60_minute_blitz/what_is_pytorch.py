ea204e43d253aeaa5173240cf5a8a9228ca075fa
damvantai <tai94bn@gmail.com>
2017-08-26 07:37:14 +0700

    Converting numpy array to torch Tensor

--- deep_learning_with_pytorch_a_60_minute_blitz/what_is_pytorch.py
+++ deep_learning_with_pytorch_a_60_minute_blitz/what_is_pytorch.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import torch
+import numpy as np
 
 x = torch.Tensor(5, 3)
 print("x: {}".format(x))
@@ -43,5 +44,12 @@ b = a.numpy()
 print(b)
 
 a.add_(1)
+print(a)
+print(b)
+
+# Converting numpy Array to torch Tensor
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out=a)
 print(a)
 print(b)
